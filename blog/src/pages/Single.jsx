c092c0ed13d345a8fad6a093aca537811fcfe1d1
MJephat <mukundijephat22@gmail.com>
2023-12-21 17:24:59 +0300

    done front end and styling of the single component

--- blog/src/pages/Single.jsx
+++ blog/src/pages/Single.jsx
@@ -1,7 +1,59 @@
 import React from 'react'
+import {Link} from 'react-router-dom'
+import Edit from '../image/edit.png'
+import Del from '../image/delete.png'
+import Menu from '../components/menu'
 
 export const Single = () => {
   return (
-    <div>Single</div>
+    <div className="single">
+      <div className="content">
+        <img src='https://images.unsplash.com/photo-1607224431233-1baa09f6f56d?w=400&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTh8fHNwbGFzaHxlbnwwfHwwfHx8MA%3D%3D' alt=''/>
+      <div className="user">
+        <img src='https://images.unsplash.com/photo-1599566150163-29194dcaad36?w=400&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTl8fHBlcnNvbnxlbnwwfHwwfHx8MA%3D%3D' alt=''/>
+      <div className="info">
+        <span>John</span>
+        <p>Posted 2 days ago</p>
+      </div>
+      <div className="edit">
+        <Link to={`/write?edit=2`}>
+          <img src={Edit} alt=''/>
+        </Link>
+        <img src={Del} alt=''/>
+      </div>
+      </div>
+      <h1>Lorem ipsum dolor sit amet, consectetur adipiscing elit</h1>
+      <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque 
+        laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto
+        beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut
+         odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt.
+         <br/><br/>
+         <br />
+          Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet,
+        consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore
+           quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? 
+           Quis autem vel eum iure reprehenderit qui in ea voluptate
+            velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatu
+         <br/><br/>
+         <br />
+            Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque 
+        laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto
+        beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut
+         odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt.
+          Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet,
+        consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore
+        <br/><br/>
+         <br />
+           quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? 
+           Quis autem vel eum iure reprehenderit qui in ea voluptate
+            velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatu?
+         
+
+            </p>
+      </div>
+      <div className="menu">
+        <Menu />
+      </div>
+    </div>
   )
 }
