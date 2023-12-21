eba0c678fe19e0fc7efde8209ededb82a48228d0
MJephat <mukundijephat22@gmail.com>
2023-12-21 10:34:38 +0300

    navigition bar worked on

--- blog/src/App.js
+++ blog/src/App.js
@@ -6,6 +6,7 @@ import Write from "./pages/Write";
 import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
 import Footer from "./components/Footer";
+import "./style.scss"
 
 const Layout = () => {
     return(
