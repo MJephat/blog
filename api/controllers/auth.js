3fcf8a832c783c8d66d8d145e4bc6064a400f090
MJephat <mukundijephat22@gmail.com>
2023-12-27 21:00:19 +0300

    authentication done

--- api/controllers/auth.js
+++ api/controllers/auth.js
@@ -55,5 +55,10 @@ export const login = (req, res) =>{
 }
 
 export const logout = (req, res) =>{
+
+    res.clearCookie("access_token",{
+        sameSite:"none",
+        secure:true
+    }).status(200).json("User logged out")
     
 }
--- blog/src/components/Navbar.jsx
+++ blog/src/components/Navbar.jsx
@@ -1,9 +1,14 @@
-import React from 'react'
-import {Link} from 'react-router-dom'
-import Logo from '../image/logo1.png'
+import React,{ useContext } from 'react';
+import {Link} from 'react-router-dom';
+import Logo from '../image/logo1.png';
+import { AuthContext } from '../context/authContext';
+
 
 
 const Navbar = () => {
+
+  const { currentUser,logout } = useContext(AuthContext);
+
   return (
     <div className='navbar'>
         <div className="container">
@@ -17,8 +22,12 @@ const Navbar = () => {
                 <Link className='link' to="/?cat=cinema"><h6>CINEMA</h6> </Link>
                 <Link className='link' to="/?cat=design"><h6>DESIGN</h6> </Link>
                 <Link className='link' to="/?cat=food"><h6>FOOD</h6> </Link>
-                <span>John</span>
-                <span>Logout</span>
+                <span>{currentUser?.username }</span>
+                {currentUser ?(<span onClick={logout}>Logout</span>) :(
+                  <Link className='link' to="/login">
+                    Login
+                  </Link>
+                )}
                 <span className='write'>
                     <Link className="link" to="/write">Write</Link>
                 </span>
