1319b6297d04febc95944ea67008c5fbb741d86d
MJephat <mukundijephat22@gmail.com>
2023-12-27 19:43:44 +0300

    local storage for cookies and session  for login and logout function

--- blog/src/pages/Login.jsx
+++ blog/src/pages/Login.jsx
@@ -1,7 +1,9 @@
 import React from 'react'
 import { Link, useNavigate } from 'react-router-dom'
 import { useState } from 'react'
-import axios from 'axios'
+import { useContext } from 'react'
+import { AuthContext } from '../context/authContext'
+// import axios from 'axios'
 
 
 const Login = () => {
@@ -12,6 +14,10 @@ const Login = () => {
   const [err,setError] = useState(null);
   const navigate = useNavigate()
 
+  const { login } = useContext(AuthContext);
+
+  // console.log(currentUser)
+
 
   const handleChange = e =>{
     setInputs(prev=> ({...prev, [e.target.name]: e.target.value}))
@@ -21,7 +27,8 @@ const Login = () => {
   const handleSubmit = async e =>{
     e.preventDefault()
     try{
-      await axios.post("/auth/login", inputs)
+      // await axios.post("/auth/login", inputs)
+      await login(inputs)
       navigate("/")
     } catch(err){
       // console.log(err)
