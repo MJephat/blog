5942d5a08a3c009b8ea3afac9e23ee19f30ab99f
MJephat <mukundijephat22@gmail.com>
2023-12-27 12:21:20 +0300

    connected with database and created register route

--- api/index.js
+++ api/index.js
@@ -1,9 +1,17 @@
 import express from "express"
+import postRoutes from "./routes/posts.js"
+import authRoutes from "./routes/auth.js"
+import userRoutes from "./routes/users.js"
+
 
 const app = express()
 
 app.use(express.json())
+app.use("/api/posts", postRoutes)
+app.use("/api/users", userRoutes)
+app.use("/api/auth", authRoutes)
 
-app.listen(8800, ()=>{
-    console.log("Connected!")
+
+app.listen(8080,() =>{
+    console.log("Connected")
 })
