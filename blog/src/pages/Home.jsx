17625259e2beccab10cfb405afdae29b17a1ef87
MJephat <mukundijephat22@gmail.com>
2024-01-03 10:49:36 +0300

    worked on dynamic single page component

--- blog/src/pages/Home.jsx
+++ blog/src/pages/Home.jsx
@@ -1,46 +1,65 @@
-import React from 'react'
-import {Link} from 'react-router-dom'
+import React from 'react';
+import { useEffect } from 'react';
+import { useState } from 'react';
+import { Link, useLocation } from 'react-router-dom';
+import axios from 'axios';
 
 const Home = () => {
+    const [posts,setPosts] = useState([]);
 
-    const posts =[
-        {
-            id: 1,
-            title: "Lorem ipsum dolor.",
-            desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.",
-            img:"https://images.unsplash.com/photo-1623855244183-52fd8d3ce2f7?w=400&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxjb2xsZWN0aW9uLXBhZ2V8MTJ8MzU1NDQ2Nnx8ZW58MHx8fHx8"
-        },
-        {
-            id: 2,
-            title: "Lorem ipsum dolor.",
-            desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.",
-            img:"https://images.unsplash.com/photo-1541348263662-e068662d82af?w=400&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxjb2xsZWN0aW9uLXBhZ2V8MXwzODAyMjcyfHxlbnwwfHx8fHw%3D"
-        },
-        {
-            id: 3,
-            title: "Lorem ipsum dolor sit.",
-            desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.",
-            img:"https://media.istockphoto.com/id/1440495095/photo/laboratory-investigations-concerning-test-and-medicine-against-covid.jpg?s=612x612&w=is&k=20&c=AA1AEXvAVK20Fg9qKLW0JolKeunKqzA2mlJUx7NnY2s="
-        },  
-        {
-            id: 4,
-            title: "Lorem ipsum dolor sit.",
-            desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.",
-            img:"https://images.unsplash.com/photo-1608403890614-ec62cb35b46e?w=400&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxjb2xsZWN0aW9uLXBhZ2V8MXw1NTk1Mjd8fGVufDB8fHx8fA%3D%3D"
-        }, 
-        {
-            id: 5,
-            title: "Lorem ipsum dolor.",
-            desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.",
-            img:"https://images.unsplash.com/flagged/photo-1615733616826-9f14b6f1eff2?w=400&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxjb2xsZWN0aW9uLXBhZ2V8MXx1djJ4eDk1LWdpY3x8ZW58MHx8fHx8"
-        },
-        {
-            id: 6,
-            title: "Lorem ipsum dolor.",
-            desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.",
-            img:"https://images.unsplash.com/photo-1684182413379-7f1ee71b021b?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxjb2xsZWN0aW9uLXBhZ2V8MXw3NzE5NzkzNXx8ZW58MHx8fHx8"
-        },
-    ];
+    const cat = useLocation().search
+
+    useEffect(()=>{
+        const fetchData =  async () =>{
+            try{
+                const res = await axios.get(`/posts${cat}`);
+                setPosts(res.data);
+            }catch(err){
+                console.log(err);
+            }
+        };
+        fetchData();
+    }, [cat]);
+
+    // const posts =[
+    //     {
+    //         id: 1,
+    //         title: "Lorem ipsum dolor.",
+    //         desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.",
+    //         img:"https://images.unsplash.com/photo-1623855244183-52fd8d3ce2f7?w=400&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxjb2xsZWN0aW9uLXBhZ2V8MTJ8MzU1NDQ2Nnx8ZW58MHx8fHx8"
+
+    //     },
+    //     {
+    //         id: 2,
+    //         title: "Lorem ipsum dolor.",
+    //         desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.",
+    //         img:"https://images.unsplash.com/photo-1541348263662-e068662d82af?w=400&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxjb2xsZWN0aW9uLXBhZ2V8MXwzODAyMjcyfHxlbnwwfHx8fHw%3D"
+    //     },
+    //     {
+    //         id: 3,
+    //         title: "Lorem ipsum dolor sit.",
+    //         desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.",
+    //         img:"https://media.istockphoto.com/id/1440495095/photo/laboratory-investigations-concerning-test-and-medicine-against-covid.jpg?s=612x612&w=is&k=20&c=AA1AEXvAVK20Fg9qKLW0JolKeunKqzA2mlJUx7NnY2s="
+    //     },  
+    //     {
+    //         id: 4,
+    //         title: "Lorem ipsum dolor sit.",
+    //         desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.",
+    //         img:"https://images.unsplash.com/photo-1608403890614-ec62cb35b46e?w=400&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxjb2xsZWN0aW9uLXBhZ2V8MXw1NTk1Mjd8fGVufDB8fHx8fA%3D%3D"
+    //     }, 
+    //     {
+    //         id: 5,
+    //         title: "Lorem ipsum dolor.",
+    //         desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.",
+    //         img:"https://images.unsplash.com/flagged/photo-1615733616826-9f14b6f1eff2?w=400&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxjb2xsZWN0aW9uLXBhZ2V8MXx1djJ4eDk1LWdpY3x8ZW58MHx8fHx8"
+    //     },
+    //     {
+    //         id: 6,
+    //         title: "Lorem ipsum dolor.",
+    //         desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.",
+    //         img:"https://images.unsplash.com/photo-1684182413379-7f1ee71b021b?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxjb2xsZWN0aW9uLXBhZ2V8MXw3NzE5NzkzNXx8ZW58MHx8fHx8"
+    //     },
+    // ];
 
   return (
     <div className="home">
@@ -60,9 +79,8 @@ const Home = () => {
                 </div>
             ))}
         </div>
-
     </div>
-  )
-}
+  );
+};
 
 export default Home
--- blog/src/pages/Single.jsx
+++ blog/src/pages/Single.jsx
@@ -1,55 +1,55 @@
-import React from 'react'
-import {Link} from 'react-router-dom'
+import React, { useState, useEffect, useContext } from 'react'
+import {Link, useLocation} from 'react-router-dom'
 import Edit from '../image/edit.png'
 import Del from '../image/delete.png'
 import Menu from '../components/menu'
+import moment from 'moment'
+import axios from 'axios'
+import { AuthContext } from '../context/authContext'
 
 export const Single = () => {
+  const [post,setPost] = useState({});
+
+  const location = useLocation()
+
+  const postId = location.pathname.split("/")[2]
+
+  const {currentUser} = useContext(AuthContext)
+
+  useEffect(()=>{
+      const fetchData =  async () =>{
+          try{
+              const res = await axios.get(`/posts/${postId}`);
+              setPost(res.data);
+          }catch(err){
+              console.log(err);
+          }
+      };
+      fetchData();
+  }, [postId]);
+
   return (
     <div className="single">
       <div className="content">
-        <img src='https://images.unsplash.com/photo-1607224431233-1baa09f6f56d?w=400&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTh8fHNwbGFzaHxlbnwwfHwwfHx8MA%3D%3D' alt=''/>
+        <img src={post?.img} alt=''/>
       <div className="user">
         <img src='https://images.unsplash.com/photo-1599566150163-29194dcaad36?w=400&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTl8fHBlcnNvbnxlbnwwfHwwfHx8MA%3D%3D' alt=''/>
       <div className="info">
-        <span>John</span>
-        <p>Posted 2 days ago</p>
+        <span>{post.username}</span>
+        <p>Posted {moment(post.date).fromNow()}</p>
       </div>
+      {currentUser.username === post.username && (
       <div className="edit">
         <Link to={`/write?edit=2`}>
           <img src={Edit} alt=''/>
         </Link>
         <img src={Del} alt=''/>
+      </div>)}
       </div>
-      </div>
-      <h1>Lorem ipsum dolor sit amet, consectetur adipiscing elit</h1>
-      <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque 
-        laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto
-        beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut
-         odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt.
-         <br/><br/>
-         <br />
-          Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet,
-        consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore
-           quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? 
-           Quis autem vel eum iure reprehenderit qui in ea voluptate
-            velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatu
-         <br/><br/>
-         <br />
-            Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque 
-        laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto
-        beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut
-         odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt.
-          Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet,
-        consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore
-        <br/><br/>
-         <br />
-           quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? 
-           Quis autem vel eum iure reprehenderit qui in ea voluptate
-            velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatu?
-         
-
-            </p>
+      <h1>{post.title}</h1>
+      {/* <p> */}
+        {post.description}
+      {/* </p> */}
       </div>
       <div className="menu">
         <Menu />
